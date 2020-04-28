94f343ccb3a1d822eb11b3179234a4a5e8d8aac5
JOOKazama <parasyte@abv.bg>
2020-04-28 22:43:05 +0300

    Sixth.

---
+++ app/src/main/java/Operations/Remove.java
@@ -0,0 +1,58 @@
+package Operations;
+import Validators.Validator;
+import android.widget.Toast;
+import com.app6.MainActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import Movies.MovieSource;
+import com.app6.R;
+
+public class Remove extends Activity
+{
+    Button remove;
+    EditText delete;
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delete);
+        delete=findViewById(R.id.delete);
+        remove=findViewById(R.id.remove);
+        this.setTitle("Remove");
+
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int wp=metrics.widthPixels;
+        int hp=metrics.heightPixels;
+
+        getWindow().setLayout((wp), (int)(hp*0.3));
+
+        remove.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View v)
+            {
+                Validator.valremove(delete.getText().toString());
+
+                switch(Validator.error)
+                {
+                    case "Please enter a name!":
+                    case "Enter a name with less than 30 characters!":
+                    case "No such movie!":
+                    Toast.makeText(getApplicationContext(), Validator.error, Toast.LENGTH_SHORT).show();
+                    break;
+
+                    case "no":
+                    MovieSource.delete(delete.getText().toString());
+                    runOnUiThread(new Runnable() { public void run() { MainActivity.adapter.notifyDataSetChanged(); } });
+                    Toast.makeText(getApplicationContext(), "Movie successfully removed!", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/app6/Adapter.java
@@ -0,0 +1,37 @@
+package com.app6;
+import Movies.Movie;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<ViewHolder>
+{
+    private final ArrayList<Movie>movies;
+
+    public Adapter(ArrayList<Movie>movies){ this.movies=movies; }
+
+    @NonNull @Override public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        Context context=parent.getContext();
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View contactView=inflater.inflate(R.layout.showmovie, parent, false);
+        return new ViewHolder((contactView));
+    }
+
+    @Override public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        final Movie m=movies.get(position);
+        holder.setName("Name: "+m.getName());
+        holder.setDirector("Director: "+m.getDirector());
+        holder.setGenre("Genre: "+m.getGenre());
+        holder.setYear("Year: "+m.getYear());
+        holder.setRating(m.getRating());
+        holder.setIndex("Index: "+holder.getAdapterPosition());
+    }
+
+    @Override public int getItemCount() { return movies.size(); }
+}
